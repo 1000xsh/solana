09e01e026e821bb21f4a152037c5116c00a5a5e8
1000x.sh <142979932+1000xsh@users.noreply.github.com>
2024-04-24 09:49:56 +0200

    create set_affinity.sh

---
+++ scripts/set_affinity.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# wait until the ledger is loaded, bc there will be no thread id until then.
+# check logs with: journalctl -xe
+# wait to load the binary
+# sleep 20
+
+# main pid of solana-validator
+solana_pid=$(pgrep -f "^solana-validator --identity")
+if [ -z "$solana_pid" ]; then
+    logger "set_affinity: solana_validator_404"
+    exit 1
+fi
+
+# find thread id
+thread_pid=$(ps -T -p $solana_pid -o spid,comm | grep 'solPohTickProd' | awk '{print $1}')
+if [ -z "$thread_pid" ]; then
+    logger "set_affinity: solPohTickProd_404"
+    exit 1
+fi
+
+# check if aff already set
+current_affinity=$(taskset -cp $thread_pid 2>&1 | awk '{print $NF}')
+if [ "$current_affinity" == "2" ]; then
+    logger "set_affinity: solPohTickProd_already_set"
+    exit 1
+else
+    # set poh to cpu2
+    sudo taskset -cp 2 $thread_pid
+    logger "set_affinity: set_done"
+     # $thread_pid
+fi
